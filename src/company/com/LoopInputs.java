a14f1c9e9e7542465c16daa2eb1bccfde4d80ca9
malav999 <malavshah2018@gmail.com>
2021-04-22 17:23:58 -0400

    Task3 LoopInputs

--- src/company/com/LoopInputs.java
+++ src/company/com/LoopInputs.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class LoopInputs {
     public static void main(String[] args) {
+        System.out.println("Start Entering 10 numbers one by one to get the average");
         Scanner sc = new Scanner(System.in);
+        int sum = 0;
         for(int i = 0; i<10; i++){
-            System.out.println("Enter a number");
-            System.out.println("Thanks for entering " + sc.nextInt());
+            int n = sc.nextInt();
+            System.out.println("You entered " + n);
+            sum +=n;
         }
-        System.out.println("You have entered 10 numbers.");
+
+        System.out.println("Average of entered 10 numbers in " + sum/10);
     }
 }

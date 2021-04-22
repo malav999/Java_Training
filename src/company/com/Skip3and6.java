55ed5f5d97e3103f363477bd97fe66111301284a
malav999 <malavshah2018@gmail.com>
2021-04-21 23:33:49 -0400

    Task2 Skip 3 and 6

---
+++ src/company/com/Skip3and6.java
@@ -0,0 +1,11 @@
+package company.com;
+
+public class Skip3and6 {
+    public static void main(String args[]){
+        for(int i = 0; i<7; i++){
+            if(i!=3 && i!= 6){
+                System.out.println(i);
+            }
+        }
+    }
+}

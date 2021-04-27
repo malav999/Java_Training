0b22bff120ec7fbff6851f687bcfc50b162c9e04
malav999 <malavshah2018@gmail.com>
2021-04-27 17:07:46 -0400

    Task4 Second smallest element in the array

---
+++ src/company/com/SecondSmallest.java
@@ -0,0 +1,17 @@
+package company.com;
+
+public class SecondSmallest {
+    public static void main(String[] args) {
+        int arr [] = new int []{1,9,7,5,2,5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j = i+1; j<arr.length; j++)
+                if(arr[i]<arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+        }
+        System.out.println("Second smallest element in the given array is -: "+arr[arr.length - 2]);
+    }
+}

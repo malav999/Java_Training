90dd11054840c84dd69bbd06c7b291f3024e8492
malav999 <malavshah2018@gmail.com>
2021-04-27 14:08:25 -0400

    Task3 Pascal's Triangle

---
+++ src/company/com/Pyramid.java
@@ -0,0 +1,26 @@
+package company.com;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        System.out.println("Enter no of rows to form right angle triangle");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int spaces = rows;
+        int k = 1;
+        for(int i = 0; i < rows; i++) {
+            for(int space = 1; space <= spaces; space++) {
+                System.out.print(" ");
+            }
+            k = 1;
+            for(int j = 0; j <= i; j++) {
+                System.out.print(k + " ");
+                k = k * (i-j)/(j+1);
+            }
+            spaces--;
+
+            System.out.println();
+        }
+    }
+}

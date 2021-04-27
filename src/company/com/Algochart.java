b284104725c09651bc09c5cb07c9b5cea5cbe1bd
malav999 <malavshah2018@gmail.com>
2021-04-27 12:18:53 -0400

    Task2 Algo Chart

--- src/company/com/Algochart.java
+++ src/company/com/Algochart.java
@@ -3,7 +3,6 @@ package company.com;
 public class Algochart {
     public static void main(String[] args){
         int a=10,b=20,c=30;
-
         float avg=(a+b+c)/3;
 
         System.out.println("avg = " +avg);
